4eff8dcad59c6ec089ed99883edde90716b5e546
Viviana Mardones <vivi.mardonesh@gmail.com>
2026-01-19 22:24:14 -0300

    Se modifican las imagenes

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -1,21 +1,30 @@
-import { Typography, Button, Container } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Typography, Container } from "@mui/material";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "../styles/Inicio.css"; 
+import "../styles/Inicio.css";
 
 import patin1 from "../images/patin_1.jpg";
 import patin2 from "../images/patin_2.jpg";
 import patin3 from "../images/patin_3.jpg";
 
-<Container maxWidth="sm">
-  
-</Container>
+<Container maxWidth="sm"></Container>;
 const noticias = [
-  { titulo: "Campeonato Nacional", resumen: "Speed Project arrasa en la final.", imagen: patin1 },
-  { titulo: "Nueva indumentaria", resumen: "Presentamos el nuevo uniforme oficial.", imagen: patin2 },
-  { titulo: "Entrenamiento abierto", resumen: "Ven a conocer el club.", imagen: patin3 },
+  {
+    titulo: "Campeonato Nacional",
+    resumen: "Speed Project arrasa en la final.",
+    imagen: patin1,
+  },
+  {
+    titulo: "Nueva indumentaria",
+    resumen: "Presentamos el nuevo uniforme oficial.",
+    imagen: patin2,
+  },
+  {
+    titulo: "Entrenamiento abierto",
+    resumen: "Ven a conocer el club.",
+    imagen: patin3,
+  },
 ];
 
 export default function Inicio() {
@@ -31,30 +40,22 @@ export default function Inicio() {
   return (
     <div>
       {/* Banner principal */}
-      <div
-        className="banner"
-        style={{ backgroundImage: `url(${patin1})` }} // 👈 usa una imagen como fondo
-      >
+      <div className="banner">
         <Typography variant="h4" align="center">
-          ONE TEAM, ONE DREAM 🏁
+          Speed Project Chile
         </Typography>
       </div>
 
       {/* Encabezado */}
       <Container className="encabezado">
-        <Typography variant="h3" gutterBottom>
-          Club Speed Project
-        </Typography>
-        <Typography variant="subtitle1" gutterBottom>
-          Una pasión, un equipo, un sueño
-        </Typography>
-
         {/* Carrusel */}
         <div className="carrusel">
           <Slider {...sliderSettings}>
             {noticias.map((n, i) => (
-              <div key={i}>
-                <img src={n.imagen} alt={n.titulo} />
+              <div key={i} className="carrusel-slide">
+                <div className="carrusel-img-container">
+                  <img src={n.imagen} alt={n.titulo} className="carrusel-img" />
+                </div>
                 <Typography variant="h6" align="center" sx={{ mt: 2 }}>
                   {n.titulo}
                 </Typography>
@@ -64,13 +65,6 @@ export default function Inicio() {
               </div>
             ))}
           </Slider>
-        </div>
-
-        {/* Botón de navegación */}
-        <div className="boton-noticias">
-          <Button variant="contained" color="primary" component={Link} to="/noticias">
-            Ver más noticias
-          </Button>
         </div>
       </Container>
     </div>
